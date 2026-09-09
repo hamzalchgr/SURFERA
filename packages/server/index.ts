1e9b4c5b0d7169c9d7dfb746c78567ecc71f912c
hamzalchgr <hamzalchgrdev@gmail.com>
2026-09-09 10:10:59 +0200

    Starting an express js files.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import express from 'express';
+import type { Express } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app: Express = express();
+
+app.use(cors);
+app.use(express.json());
+
+app.listen(PORT, () => {
+   console.log('Running ...')
+})
